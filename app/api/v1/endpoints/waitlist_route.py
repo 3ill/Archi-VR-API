8eb146bc4707593a89f76fe36bdc48dcd6459c40
3illBaby <thrillanon@outlook.com>
2026-07-12 17:11:08 +0100

    chore(waitlist_route.py): init route file

--- app/api/v1/endpoints/waitlist_route.py
+++ app/api/v1/endpoints/waitlist_route.py
@@ -2,9 +2,12 @@ from functools import lru_cache
 
 from fastapi import APIRouter
 
+from app.db.db import async_session_maker
+from app.services.waitlist.waitlist_service import WaitlistService
+
 router = APIRouter()
 
 
 @lru_cache(maxsize=1)
 def get_waitlist_service():
-    return None
+    return WaitlistService(async_session_maker)
