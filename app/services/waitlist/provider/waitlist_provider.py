f926739433aaa13785b2bc6350d8f6700d471caa
3illBaby <thrillanon@outlook.com>
2026-07-12 17:12:28 +0100

    chore(waitlist_provider.py): updated provider

--- app/services/waitlist/provider/waitlist_provider.py
+++ app/services/waitlist/provider/waitlist_provider.py
@@ -10,12 +10,12 @@ from app.services.waitlist.provider.base import BaseWaitlistProvider
 
 class WaitlistProvider(BaseWaitlistProvider):
     def __init__(self, async_sessionmaker: async_sessionmaker) -> None:
-        self._sessionmaker = async_sessionmaker
+        self._session_maker = async_sessionmaker
         self._logger = logging.getLogger(__name__)
         super().__init__()
 
     async def add_user_to_waitlist(self, ctx: AddToWaitlistDto) -> Waitlist | None:
-        async with self._sessionmaker() as session:
+        async with self._session_maker() as session:
             waitlist = Waitlist(
                 first_name=ctx.first_name, last_name=ctx.last_name, email=ctx.email
             )
@@ -27,14 +27,14 @@ class WaitlistProvider(BaseWaitlistProvider):
             return waitlist
 
     async def get_waitlist_by_email(self, email: str) -> Waitlist | None:
-        async with self._sessionmaker() as session:
+        async with self._session_maker() as session:
             stmt = select(Waitlist).where(Waitlist.email == email)
             result = await session.execute(stmt)
             waitlist = result.scalar_one_or_none()
             return waitlist
 
     async def get_waitlist(self) -> list[Waitlist]:
-        async with self._sessionmaker() as session:
+        async with self._session_maker() as session:
             stmt = select(Waitlist)
             result = await session.execute(stmt)
             waitlist = result.scalars().all()
