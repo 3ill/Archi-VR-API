a0e942e4508e20bbd7ce08f624995d160feae5dc
3illBaby <thrillanon@outlook.com>
2026-07-12 12:22:45 +0100

    chore(shared_dto.py): added dto

---
+++ app/shared/dto/shared_dto.py
@@ -0,0 +1,15 @@
+from typing import Any, Callable
+
+from pydantic import BaseModel, ConfigDict
+
+
+class EventSubscribeDto(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    event_name: str
+    listener: Callable[..., Any]
+
+
+class PublishEventDto(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    event_name: str
+    payload: Any
