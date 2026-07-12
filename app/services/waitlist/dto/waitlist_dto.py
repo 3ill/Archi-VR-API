66e4c1ad1d9993ba93e50a9e7eb1e61eb61d6014
3illBaby <thrillanon@outlook.com>
2026-07-12 17:12:03 +0100

    chore(waitlist_dto.py): updated dto

--- app/services/waitlist/dto/waitlist_dto.py
+++ app/services/waitlist/dto/waitlist_dto.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, ConfigDict
 
+from app.shared.dto.shared_dto import SuccessResponseDto
+
 
 class BaseWaitlistDto(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -9,3 +11,11 @@ class AddToWaitlistDto(BaseWaitlistDto):
     email: str
     first_name: str
     last_name: str
+
+
+class AddToWaitlistData(BaseWaitlistDto):
+    id: str
+
+
+class AddToWaitlistResponseDto(SuccessResponseDto):
+    data: AddToWaitlistData
