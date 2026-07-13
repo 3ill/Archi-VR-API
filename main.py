9525f5a1ddb6333592ed8c91c2cb771fa65a16cf
3illBaby <thrillanon@outlook.com>
2026-07-13 05:47:29 +0100

    chore(main.py): moved port to 3000

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from uvicorn import run
 
 
 def initialize():
-    run("app.app:app", host="0.0.0.0", port=8000, reload=True)
+    run("app.app:app", host="0.0.0.0", port=3000, reload=True)
 
 
 if __name__ == "__main__":
