54c0c1f30746bf23e84c2675fbd6fc0273cf3a4f
3illBaby <thrillanon@outlook.com>
2026-07-16 06:55:47 +0100

    feat(router.py): registered route

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import waitlist_route
+from app.api.v1.endpoints import asset_route, waitlist_route
 
 api_router = APIRouter()
 
 
 api_router.include_router(waitlist_route.router, prefix="/waitlist", tags=["waitlist"])
+api_router.include_router(asset_route.router, prefix="/asset", tags=["asset"])
