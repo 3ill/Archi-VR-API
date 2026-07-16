ba31e674f06c6c7d498a16ce0b4d38f069e78580
3illBaby <thrillanon@outlook.com>
2026-07-16 18:58:23 +0100

    config(app_config.py): extended config

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -3,3 +3,5 @@ from pydantic_settings import BaseSettings
 
 class AppConfig(BaseSettings):
     APP_UPLOAD_URL: str = "https://archivr-visualizer.vercel.app/upload"
+    INSTAGRAM_URL: str = "https://www.instagram.com/polarbear_xr"
+    LINKEDIN_URL: str = "https://www.linkedin.com/company/polar-bear-vr/"
